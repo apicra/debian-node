20b2e41d2e40590e053890c742b851af1a27415d
tom <tom@sapletta.com>
2020-05-05 16:19:31 +0200

    update

---
+++ stop.sh
@@ -0,0 +1,6 @@
+#To kill all PHP Processes
+kill $(ps aux | grep '[p]hp' | awk '{print $2}')
+#To kill all Nginx Processes
+kill $(ps aux | grep '[n]ginx' | awk '{print $2}')
+#To kill all MySQL Processes
+kill $(ps aux | grep '[m]ysql' | awk '{print $2}')
