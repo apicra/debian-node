9e843d8ddb2fd7099b78bf737fb7affddc75b60a
tom <tom@sapletta.com>
2020-05-17 16:07:26 +0200

    update

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-apt autoremove
+apt autoremove -y
 apt update
-apt install nodejs npm
+apt install nodejs npm -y
 #curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
 #apt install nodejs
